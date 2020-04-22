d9562282dfb5c926ece32174c44d8b89a43dbeb2
Rishi Rao <22raor@gmail.com>
2020-04-22 02:08:29 -0400

    Move to ciphers package

--- src/main/java/ciphers/Vigenere.java
+++ src/main/java/ciphers/Vigenere.java
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  * 
  */
-package cipher;
+package ciphers;
 
 /**
  * This class provides the necessary methods to encrypt and decrypt messages
